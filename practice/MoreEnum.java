a912af725331878cd4766acfb1c74269f099a6db
Dhruv <dhruvarora2612@gmail.com>
2026-04-17 01:19:59 +0530

    files added

--- practice/MoreEnum.java
+++ practice/MoreEnum.java
@@ -1,6 +1,29 @@
+enum Laptop{
+    Macbook(2000),Dell(1500),HP,Lenovo(1200);
+
+    private int price;
+
+    Laptop(){}
+
+    Laptop(int price){
+        this.price = price;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+}
 
 public class MoreEnum {
     public static void main(String[] args) {
-        
+        Laptop lap = Laptop.Dell;
+        System.out.println(lap + " costs $" + lap.getPrice());
+
+        Laptop[] arr = Laptop.values();
+        for(Laptop i: arr){
+            System.out.println(i + " costs $" + i.getPrice());
+        }
     }
 }
